1af40a121f6aad52afe984ebcc380e34b3078860
Pavan Kumar <mragpavank@gmail.com>
2022-07-07 13:45:32 +0530

    Add files via upload

---
+++ sql1.sql
@@ -0,0 +1,13 @@
+/* Day - 1
+#How to create the database
+create database employee;
+#how to show the databases
+show databases;
+#how to use the database?
+use employee;
+#how to delete the database
+drop database employee;
+show databases
+*/
+
+
